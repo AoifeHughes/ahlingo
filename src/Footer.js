f55a79b1972743c83f0d1303815ff23dd197462d
AoifeHughes <ahughes@turing.ac.uk>
2023-10-17 08:25:08 +0100

    added some basics

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="http://github.com/aoifehughes/ahlingo" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+      <p>MIT License | Free forever for everyone.</p>
+    </footer>
+  );
+}
+
+export default Footer;
