ef488fddff9b0c99d604812409bfe1b316ea9633
AoifeHughes <ahughes@turing.ac.uk>
2023-11-10 20:48:09 +0000

    run n times now

--- content/creation/french/generate_via_server.sh
+++ content/creation/french/generate_via_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define the number of runs for each prompt
+number_of_runs=5
+
 # Start Ollama server
 ollama serve &
 SERVER_PID=$!
@@ -20,33 +23,45 @@ process_prompt() {
     local PROMPTS_FILE="$1"
     local LEVEL=$(dirname "$PROMPTS_FILE" | xargs basename)
     local BASENAME=$(basename "$PROMPTS_FILE" .txt)
-
+    
     # Ensure level-specific output directory exists
     mkdir -p "${OUTPUT_DIR}${LEVEL}"
 
-    # Set output file name
-    OUTPUT_FILE="${OUTPUT_DIR}${LEVEL}/${BASENAME}_response.json"
+    for run in $(seq 1 $number_of_runs); do
+        # Set output file name with run number
+        OUTPUT_FILE="${OUTPUT_DIR}${LEVEL}/${BASENAME}_run_${run}_response.json"
 
-    # Prepare data for POST request
-    local DATA=$(cat "$PROMPTS_FILE" | jq -Rs '{model: "'$MODEL_NAME'", prompt: ., stream: false}')
+        # Prepare data for POST request
+        local DATA=$(cat "$PROMPTS_FILE" | jq -Rs '{model: "'$MODEL_NAME'", prompt: ., stream: false, options: {num_predict: 512, num_ctx: 8192}}')
 
-    # Send prompt to Ollama server and capture the entire output
-    local FULL_RESPONSE=$(curl --silent --request POST \
-                                   --url http://${SERVER_HOST}:${SERVER_PORT}/api/generate \
-                                   --header "Content-Type: application/json" \
-                                   --data "$DATA")
+        # Send prompt to Ollama server and capture the entire output
+        local FULL_RESPONSE=$(curl --silent --request POST \
+                                       --url http://${SERVER_HOST}:${SERVER_PORT}/api/generate \
+                                       --header "Content-Type: application/json" \
+                                       --data "$DATA")
 
-    # Extract only the 'response' part of the output
-    echo "$FULL_RESPONSE" | jq -r '.response' | sed 's/\\n/\n/g' > "$OUTPUT_FILE"
-
+        # Extract only the 'response' part of the output
+        echo "$FULL_RESPONSE" | jq -r '.response' | sed 's/\\n/\n/g' > "$OUTPUT_FILE"
+    done
 }
 
-./generate_prompts.sh
+./make_prompts.sh
+
+# Count the total number of .txt files
+total_files=$(find "$PROMPTS_DIR" -type f -name "*.txt" | wc -l)
+counter=0
 
 # Iterate over all .txt files in the PROMPTS_DIR and process each one
 find "$PROMPTS_DIR" -type f -name "*.txt" | while read FILE; do
     process_prompt "$FILE"
+
+    # Update and display the progress
+    ((counter++))
+    percent=$((counter * 100 / total_files))
+    printf "Processing: %d%% (%d/%d)\r" $percent $counter $total_files
 done
+
+echo "Processing complete."
 
 ./clean_json.sh
 mkdir -p ../lessons/french
@@ -56,6 +71,7 @@ target_base="../lessons/french"
 for dir in */; do
     mkdir -p "${target_base}/${dir}"
     find "$dir" -name "*.json" -exec mv {} "${target_base}/${dir}" \;
+    rm -rf "$dir"
 done
 
 # Stop the server
