11c053d876872ae146fa3c1d643ab37c84cea078
AoifeHughes <ahughes@turing.ac.uk>
2023-11-13 09:32:01 +0000

    Add IPC handlers for fetching difficulty levels, topics by difficulty, and languages by topic and difficulty. Refactor LanguageDB constructor to use absolute path. Update Writing component to Chatbot component.

--- src/main/ipcHandlers.js
+++ src/main/ipcHandlers.js
@@ -17,6 +17,39 @@ function setupIPC() {
     });
   });
 
+  ipcMain.on('get-difficulty-levels', (event) => {
+    db.getDifficultyLevels((err, levels) => {
+      if (err) {
+        console.error("Error fetching difficulty levels:", err);
+        event.reply('get-difficulty-levels-reply', { error: err.message });
+        return;
+      }
+      event.reply('get-difficulty-levels-reply', { levels });
+    });
+  });
+
+  ipcMain.on('get-topics-by-difficulty', (event, difficultyLevel) => {
+    db.getTopicsByDifficulty(difficultyLevel, (err, topics) => {
+      if (err) {
+        console.error("Error fetching topics:", err);
+        event.reply('get-topics-reply', { error: err.message });
+        return;
+      }
+      event.reply('get-topics-reply', { topics });
+    });
+  });
+
+  ipcMain.on('get-languages', (event, { topic, difficultyLevel }) => {
+    db.getLanguagesByTopicAndDifficulty(topic, difficultyLevel, (err, languages) => {
+      if (err) {
+        console.error("Error fetching languages:", err);
+        event.reply('get-languages-reply', { error: err.message });
+        return;
+      }
+      event.reply('get-languages-reply', { languages });
+    });
+  });
+
 // IPC Listener for fetching topics
 ipcMain.on('get-topics', (event, languageName) => {
   db.getTopicsByLanguage(languageName, (err, topics) => {
@@ -25,6 +58,7 @@ ipcMain.on('get-topics', (event, languageName) => {
       event.reply('get-topics-reply', { error: err.message });
       return;
     }
+    console.log("Sending topics:", topics);
     event.reply('get-topics-reply', { topics });
   });
 });
