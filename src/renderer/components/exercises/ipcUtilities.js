ea33f7c95089e6d61cdbdb74f5645b9f929bb376
AoifeHughes <ahughes@turing.ac.uk>
2024-03-16 13:27:20 +0000

    Refactor Pairs component and ipcUtilities

--- src/renderer/components/exercises/ipcUtilities.js
+++ src/renderer/components/exercises/ipcUtilities.js
@@ -1,9 +1,9 @@
 // ipcUtilities.js
-const { ipcRenderer } = window.require('electron');
+const { ipcRenderer } = window.require("electron");
 
 export const getLanguages = (setLanguagesCallback) => {
-  ipcRenderer.send('get-languages');
-  ipcRenderer.once('get-languages-response', (event, response) => {
+  ipcRenderer.send("get-languages");
+  ipcRenderer.once("get-languages-response", (event, response) => {
     const { error, languages } = response;
     if (!error) {
       setLanguagesCallback(languages);
@@ -11,9 +11,12 @@ export const getLanguages = (setLanguagesCallback) => {
   });
 };
 
-export const getDifficultiesByLanguage = (language, setDifficultiesCallback) => {
-  ipcRenderer.send('get-difficulty-by-language', { language });
-  ipcRenderer.once('get-difficulty-by-language-response', (event, response) => {
+export const getDifficultiesByLanguage = (
+  language,
+  setDifficultiesCallback
+) => {
+  ipcRenderer.send("get-difficulty-by-language", { language });
+  ipcRenderer.once("get-difficulty-by-language-response", (event, response) => {
     const { error, difficulty } = response;
     if (!error) {
       setDifficultiesCallback(difficulty);
@@ -21,19 +24,34 @@ export const getDifficultiesByLanguage = (language, setDifficultiesCallback) => 
   });
 };
 
-export const getTopicsByLanguageDifficulty = (language, difficulty, setTopicsCallback) => {
-  ipcRenderer.send('get-topics-by-language-difficulty', { language, difficulty });
-  ipcRenderer.once('get-topics-by-language-difficulty-response', (event, response) => {
-    const { error, topics } = response;
-    if (!error) {
-      setTopicsCallback(topics);
-    }
+export const getTopicsByLanguageDifficulty = (
+  language,
+  difficulty,
+  setTopicsCallback
+) => {
+  ipcRenderer.send("get-topics-by-language-difficulty", {
+    language,
+    difficulty,
   });
+  ipcRenderer.once(
+    "get-topics-by-language-difficulty-response",
+    (event, response) => {
+      const { error, topics } = response;
+      if (!error) {
+        setTopicsCallback(topics);
+      }
+    }
+  );
 };
 
-export const getRandomPairExercise = (language, difficulty, topic, setExercisesCallback) => {
-  ipcRenderer.send('get-random-pair-exercise', { language, difficulty, topic });
-  ipcRenderer.once('get-random-pair-exercise-response', (event, response) => {
+export const getRandomPairExercise = (
+  language,
+  difficulty,
+  topic,
+  setExercisesCallback
+) => {
+  ipcRenderer.send("get-random-pair-exercise", { language, difficulty, topic });
+  ipcRenderer.once("get-random-pair-exercise-response", (event, response) => {
     const { error, exercise } = response;
     if (!error) {
       setExercisesCallback(exercise);
